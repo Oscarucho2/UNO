7da0697712b0b6a837261a7e4ab42e2542010948
Oscarucho2 <oskarmartinez15@gmail.com>
2023-04-20 20:06:45 -0600

    Registro y login funcionando, agregando lobby y dirección IP al registro

--- src/loggin/LobbyController.java
+++ src/loggin/LobbyController.java
@@ -5,12 +5,15 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class LobbyController {
 
+    private Button getoutbtn;
     //Constructor
     public LobbyController() {
     } 
@@ -18,10 +21,19 @@ public class LobbyController {
     @FXML
     public void muestraLobby(ActionEvent event) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("ViewPrincipal.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ViewLobby.fxml"));
         Scene scene = new Scene(root);
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
     }
+    
+    @FXML
+    public void getout(ActionEvent event)throws IOException
+    {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+    }
+    
 }
--- src/loggin/Register.java
+++ src/loggin/Register.java
@@ -1,32 +1,36 @@
 package loggin;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import javafx.application.Application;
 import java.sql.PreparedStatement;
-import javafx.scene.Scene;
+import java.sql.SQLException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.PasswordField;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import java.sql.ResultSet;
 
 public class Register {
 
+    Stage primaryStage;
     private Connection conn;
     @FXML
     private TextField txtUserRegister;
+    @FXML
     private TextField txtEmailRegister;
     @FXML
     private PasswordField txtPasswordRegister;
     @FXML
     private Button btnRegister;
+    @FXML
+    private Button btnback;
 
     @FXML
     private void eventKey(KeyEvent event) {
@@ -49,44 +53,61 @@ public class Register {
     }
 
     @FXML
-    private void eventAction(ActionEvent event) {
+    private void eventAction(ActionEvent event) throws IOException {
         // Establecer la información de conexión
         String url = "jdbc:mysql://localhost:3306/unodatabase?serverTimezone=UTC";
         String user = "root";
         String password = "a987612345";
 
-        //Obtenemos los datos del usuario a registrar
+        // Obtener los datos del usuario
         String username = txtUserRegister.getText();
         String passwords = txtPasswordRegister.getText();
         String email = txtEmailRegister.getText();
 
-        // Crear la conexión
         try {
+            // Obtener la dirección IP de la máquina local
+            InetAddress ip = InetAddress.getLocalHost();
+            // Convertir la dirección IP a una cadena
+            String ipAddress = ip.getHostAddress();
+
+            // Crear la conexión
             Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conexión exitosa a la base de datos");
-            String insertQuery = "INSERT INTO usuarios (username, password, email) VALUES (?, ?, ?)";
-            String selectQuery = "SELECT LAST_INSERT_ID()";
+            String insertQuery = "INSERT INTO usuarios (username, password, email,ip_address) VALUES (?, ?, ?, ?)";
+
             // Preparar la consulta SQL
             PreparedStatement pstmt = conn.prepareStatement(insertQuery);
             pstmt.setString(1, username);
             pstmt.setString(2, passwords);
             pstmt.setString(3, email);
+            pstmt.setString(4, ipAddress);
             // Ejecutar la consulta SQL
             int rowsInserted = pstmt.executeUpdate();
             if (rowsInserted > 0) {
-                // Obtener el último id_usuario insertado
-                ResultSet rs = pstmt.getGeneratedKeys();
-                if (rs.next()) {
-                    int idUsuario = rs.getInt(1);
-                    System.out.println("El usuario ha sido registrado exitosamente con id " + idUsuario);
-                }
-                rs.close();
+                System.out.println("El usuario ha sido registrado exitosamente.");
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Registro exitoso!");
+                alert.setHeaderText(null);
+                alert.setContentText("Bienvenido, " + username + "!");
+                alert.showAndWait();
+                Loggin log = new Loggin();
+                log.showNewWindow();
             }
+
             // Cerrar la conexión
             conn.close();
-        } catch (SQLException ex) {
-            System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
-        }
 
+        } catch (SQLException ex) {
+            System.out.println("Error al registrar el usuario: " + ex.getMessage());
+        }
+    }
+
+    @FXML
+    public void backLoggin(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+        Loggin log = new Loggin();
+        log.showNewWindow();
     }
 }
